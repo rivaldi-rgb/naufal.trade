f15bb140e7c81633282b4a58201b8e2e2fa81c43
rivaldi-rgb <naufalrifaldi0089@gmail.com>
2025-07-08 08:53:47 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
 }
@@ -24,4 +23,17 @@ if (typing) {
     typing.textContent = words[i];
     i = (i + 1) % words.length;
   }, 2500);
+}
+
+// Tombol nav toggle (untuk mobile)
+const navToggle = document.getElementById("navToggle");
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    document.querySelector(".nav-links").classList.toggle("show");
+  });
+}
+
+// WhatsApp
+function openWhatsApp() {
+  window.open("https://wa.me/6281234567890", "_blank");
 }
