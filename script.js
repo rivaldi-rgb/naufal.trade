8f34ad0c80d3d368ec249cf682555a5f8a258e8c
rivaldi-rgb <naufalrifaldi0089@gmail.com>
2025-07-08 08:12:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function toggleDarkMode() {
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
+
+// Efek ketik otomatis
+const teks = "Bangun Masa Depan Finansialmu";
+let i = 0;
+const target = document.querySelector("h1");
+if (target) {
+  target.textContent = "";
+  function ketik() {
+    if (i < teks.length) {
+      target.textContent += teks.charAt(i);
+      i++;
+      setTimeout(ketik, 100);
+    }
+  }
+  ketik();
+}
